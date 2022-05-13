4da3146522e48b3bc3813624f3c2239a98ce029c
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-05-13 17:26:48 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,20 +1,49 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit5-08-HTML
+// May 12 2022
 
 'use strict'
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/<RESPITORY>/sw.js", {
-    scope: "/<RESPITORY>/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-08-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-08-HTML/",
   })
 }
 
 /**
- * Alerts "Hello, World!"
+ * Divides the first integer by the second integer.
  */
 function onButtonClick() {
-  alert("Hello, World!")
+  const originalIntegerOne = parseInt(document.getElementById("integerOne").value)
+  const originalIntegerTwo = parseInt(document.getElementById("integerTwo").value)
+  var integerTwo = parseInt(document.getElementById("integerTwo").value)
+  var integerOne
+  let count
+  var answer
+
+  if (originalIntegerOne < 0) {
+    integerOne = 0
+  } else {
+    integerOne = originalIntegerOne
+  }
+  if (integerOne != 0 && integerTwo > 0) {
+    for (count = 0; integerOne >= integerTwo; count++) {
+      // Calculation for + or - integers divided by + integers
+      integerOne = integerOne - integerTwo
+    }
+    answer = count + "r" + integerOne
+    document.getElementById("answer").innerHTML = "<h5>The result of " + originalIntegerOne + " ÷ " + originalIntegerTwo + " is " + answer + "</h5>"
+  } else if (integerOne != 0 && integerTwo < 0) {
+    console.log("Start Negative")
+    for (count = 0; integerOne >= integerTwo; count++) {
+      // Calculation for + or - integers divided by - integers
+      integerOne = integerOne + integerTwo
+    }
+    answer = -count + "r" + integerOne
+    document.getElementById("answer").innerHTML = "<h5>The result of " + originalIntegerOne + " ÷ " + originalIntegerTwo + " is " + answer + "</h5>"
+  } else {
+    document.getElementById("answer").innerHTML = "<h5>The result of " + originalIntegerOne + " ÷ " + originalIntegerTwo + " is undefined</h5>"
+  }
 }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var GHPATH = "/respitory"
+var GHPATH = "/ICS2O-Unit5-08-HTML"
 var APP_PREFIX = "gppwa_"
 var VERSION = "version_001"
 var URLS = [
